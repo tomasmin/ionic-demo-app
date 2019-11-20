123519f194857c2005ba8793670697dfdf666722
tomasmin <tomas.mineika@akka.eu>
2019-11-20 23:52:49 +0100

    disabled menu on login page

--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.page.html',
-  styleUrls: ['./sign-in.page.scss'],
+  selector: "app-sign-in",
+  templateUrl: "./sign-in.page.html",
+  styleUrls: ["./sign-in.page.scss"]
 })
 export class SignInPage implements OnInit {
+  constructor(public menu: MenuController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ionViewWillEnter() {
+    this.menu.enable(false);
   }
 
+  ngOnInit() {}
 }
