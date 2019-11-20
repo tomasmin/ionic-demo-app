41479e2516f629a0c41ae876e959ac5434450542
tomasmin <tomas.mineika@akka.eu>
2019-11-20 23:43:17 +0100

    sign in page html mockup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'sign-in',
+    loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
   }
 ];
 
