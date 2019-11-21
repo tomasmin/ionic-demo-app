4740ed6aef84f31bd19f74183e11fdc8f1c12cf5
tomasmin <tomas.mineika@akka.eu>
2019-11-21 17:53:53 +0100

    sidebar layout; small change to fb icon on sign in page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "sign-in",
+    pathMatch: "full"
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+    path: "home",
+    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
   },
   {
-    path: 'list',
-    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+    path: "list",
+    loadChildren: () => import("./list/list.module").then(m => m.ListPageModule)
   },
   {
-    path: 'sign-in',
-    loadChildren: () => import('./sign-in/sign-in.module').then( m => m.SignInPageModule)
+    path: "sign-in",
+    loadChildren: () =>
+      import("./sign-in/sign-in.module").then(m => m.SignInPageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,49 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
-      icon: 'home'
+      title: "Dashboard",
+      url: "/list",
+      icon: "list"
     },
     {
-      title: 'List',
-      url: '/list',
-      icon: 'list'
+      title: "Photos",
+      url: "/list",
+      icon: "photos"
+    },
+    {
+      title: "Available Missions",
+      url: "/list",
+      icon: "star-outline"
+    },
+    {
+      title: "My Missions",
+      url: "/list",
+      icon: "star"
+    },
+    {
+      title: "Chat",
+      url: "/list",
+      icon: "chatboxes"
     }
   ];
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public router: Router
   ) {
     this.initializeApp();
   }
@@ -36,5 +53,9 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  logOut() {
+    this.router.navigateByUrl("/sign-in");
   }
 }
